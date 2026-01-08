fbe0a34b6e33395dfe7e8d8cfa755d9d319ea86d
Keremcelikman <kerem.celikman@delta-yazilim.com>
2026-01-08 13:51:27 +0300

    Sepetteki toplamları düzeltelim

--- apps/ui/src/pages/carts/carts.ts
+++ apps/ui/src/pages/carts/carts.ts
@@ -1,5 +1,5 @@
 import { httpResource } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, computed, inject, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal, Signal, ViewEncapsulation } from '@angular/core';
 import { Common } from '../../services/common';
 import { cartModel } from '@e-ticaret/shared/models/cart.model'; 
 import { TrCurrencyPipe } from 'tr-currency';
@@ -15,10 +15,19 @@ import { TrCurrencyPipe } from 'tr-currency';
 })
 export default class Carts {
   readonly result = httpResource<cartModel[]>(() => {
-    const endpoint = `api/baskets?userId=${this.#common.user()!.id}`;
+    const endpoint = `api/carts?userId=${this.#common.user()!.id}`;
     return endpoint;
   });
   readonly data = computed(() => this.result.value() ?? []);
+
+  // Pricing signals
+  readonly taxRate = signal<number>(18);
+  readonly shippingCost = signal<number>(0);
+
+  readonly subtotal = computed(() => this.data().reduce((sum, item) => sum + (item.productPrice * (item.quantity ?? 1)), 0));
+  readonly taxAmount = computed(() => Math.round(this.subtotal() * this.taxRate() / 100));
+  readonly totalWithTax = computed(() => this.subtotal() + this.taxAmount() + this.shippingCost());
+  
 
   readonly #common = inject(Common);
 }
